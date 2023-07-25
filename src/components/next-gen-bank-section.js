d1e80c135f9cd8a7e08346ef1c61175a00392505
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2023-07-25 11:47:21 -0300

    added 'why choose easybank' section

--- src/components/next-gen-bank-section.js
+++ src/components/next-gen-bank-section.js
@@ -1,13 +1,13 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/react'
-import { Col, Container, Row } from 'react-bootstrap'
 import {
   customSection,
   whySection,
 } from '../assets/styles/NextGenBankSection.style'
 import RequestInviteButton from './request-invite-button'
-import bgColors from '../assets/images/bg-intro-desktop.svg'
 import phones from '../assets/images/image-mockups.png'
+import Card from './card'
+import { cardData } from '../data'
 
 const NextGenBankSection = () => {
   return (
@@ -17,7 +17,6 @@ const NextGenBankSection = () => {
           <div class='content'>
             <div class='image'>
               <img class='imageSrc' src={phones} alt='' />
-              
             </div>
             <div class='description'>
               <h1 class='title'>Next generation digital banking</h1>
@@ -32,15 +31,20 @@ const NextGenBankSection = () => {
         </div>
       </section>
       <section css={whySection}>
-        <div >
-          <div className='whyTitle'>
-            <h2>Why choose Easybank?</h2>
-            <p>
-              We leverage Open Banking to turn your bank account into your
-              finatial hub. Control your finaces like never before.
-            </p>
-          </div>
+        <div className='whyTitle'>
+          <h2>Why choose Easybank?</h2>
+          <p>
+            We leverage Open Banking to turn your bank account into your
+            finatial hub. Control your finaces like never before.
+          </p>
         </div>
+        <div className='cards'>
+          {cardData.map((data) => <Card data={data} /> )
+          }
+        </div>
+      </section>
+      <section>
+        
       </section>
     </div>
   )
