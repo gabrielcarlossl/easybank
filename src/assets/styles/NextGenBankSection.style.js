2edac25dec6af8369f0372b8a63aaad9464f5c73
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2023-07-25 10:58:20 -0300

    finished next generation section

--- src/assets/styles/NextGenBankSection.style.js
+++ src/assets/styles/NextGenBankSection.style.js
@@ -1,48 +1,100 @@
 /** @jsx jsx */
 import { css } from '@emotion/react'
 import bgColors from '../images/bg-intro-desktop.svg'
-
+import bgColorsMobile from '../images/bg-intro-mobile.svg'
+import phones from '../images/image-mockups.png'
 export const customSection = css`
-  margin-top: 117px;
-  height: 100vh;
   overflow-x: hidden;
   overflow-y: hidden;
-  .title {
-    margin-left: 14%;
+  .sectionStyle {
+    text-align: center;
+    padding-bottom: 5em;
+
+    .description {
+      align-self: end;
+      .title {
+        font-weight: 400;
+        color: #2d314d;
+        font-family: Arial, Helvetica, sans-serif;
+      }
+      .text {
+        color: #babcc4;
+      }
+    }
   }
-  .bgImg {
-    position: relative;
-    left: 7%;
-    bottom: 25%;
-    .colors {
+  .sectionStyle .text {
+    margin-bottom: 2.5em;
+  }
+  .image {
+    height: 450px;
+    margin: 0 calc(-1 * var(--wrapperSpace));
+    background: url(${bgColors}) center bottom no-repeat,
+      url(${bgColorsMobile}) center no-repeat;
+    background-size: 345px, cover;
+    margin-bottom: 1.2em;
+    @media (max-width: 500px) {
+      background-size: 570px, cover;
+    }
+  }
+  .imageSrc {
+    @media (max-width: 500px) {
       position: relative;
-      bottom: 26%;
-    }
-    @media (max-width: 1280px) and (min-width: 1200px) {
-      left: 13%;
-      bottom: 19%;
+      top: -7%;
+      width: 400px;
     }
   }
-  .phones {
-    z-index: 1;
-    position: absolute;
-    left: 20%;
-    bottom: 8%;
-    width: 1000px;
-    @media (max-width: 1500px) and (min-width: 1200px) {
-      left: 25%;
-      bottom: 18%;
-      width: 800px;
+  @media screen and (min-width: 768px) {
+    .sectionStyle {
+      text-align: left;
+      background: url(${bgColors}) left 650px top -290px no-repeat;
+      background-size: cover;
+      min-height: 750px;
+      box-sizing: border-box;
     }
-    @media (max-width: 1280px) and (min-width: 1200px) {
-      left: 13%;
-      bottom: 19%;
-      width: 800px;
+    .content {
+      position: relative;
+      display: grid;
+      align-items: center;
+      min-height: 550px;
+      grid-template-areas: 'description image';
+      grid-template-columns: 370px 1fr;
+      margin-left: 15%;
     }
-    @media (max-width: 1200px) and (min-width: 850px) {
-      left: 13%;
-      bottom: 28%;
-      width: 700px;
+    .image {
+      grid-area: image;
+      min-height: inherit;
+      position: absolute;
+      width: 500px;
+      left: 0;
+      top: 0;
+      height: 768px;
+      background: url(${phones}) left bottom no-repeat;
+      background-size: cover;
     }
+  }
+  @media screen and (min-width: 1024px) {
+    .imageSrc {
+      display: block;
+      position: relative;
+      top: -12px;
+      z-index: 1;
+      left: 80%;
+      @media (max-width: 1280px) and (min-width: 850px) {
+        left: -12%;
+      }
+    }
+    .image {
+      background: none;
+      left: 150px;
+      right: 0;
+      top: 0;
+    }
+  }
+`
+
+export const whySection = css`
+  background-color: #f3f4f6;
+  .whyTitle {
+    margin-left: 15%;
   }
 `
--- src/components/next-gen-bank-section.js
+++ src/components/next-gen-bank-section.js
@@ -3,6 +3,7 @@ import { jsx } from '@emotion/react'
 import { Col, Container, Row } from 'react-bootstrap'
 import {
   customSection,
+  whySection,
 } from '../assets/styles/NextGenBankSection.style'
 import RequestInviteButton from './request-invite-button'
 import bgColors from '../assets/images/bg-intro-desktop.svg'
@@ -10,24 +11,37 @@ import phones from '../assets/images/image-mockups.png'
 
 const NextGenBankSection = () => {
   return (
-    <div>
-      <div css={customSection}>
-        <Row style={{flexWrap: 'nowrap'}}>
-          <div className='title'>
-            <h1>Next generation digital banking</h1>
-            <p>
-              Take your financial life online. Your Easybank account will be a
-              one-stop-shop for spending, saving, budgeting, investing, and much
-              more.
-            </p>
-            <RequestInviteButton />
+    <div css={customSection}>
+      <section class='sectionStyle '>
+        <div class='wrapper'>
+          <div class='content'>
+            <div class='image'>
+              <img class='imageSrc' src={phones} alt='' />
+              
+            </div>
+            <div class='description'>
+              <h1 class='title'>Next generation digital banking</h1>
+              <p class='text'>
+                Take your finalcial life online. Your Easybank account will be a
+                one-stop-shop for spending. saving, budgeting, investing, and
+                much more.
+              </p>
+              <RequestInviteButton />
+            </div>
           </div>
-          <Col className='bgImg'>
-            <img className='colors' src={bgColors} alt="" />
-            <img className='phones' src={phones} alt="" />
-          </Col>
-        </Row>
-      </div>
+        </div>
+      </section>
+      <section css={whySection}>
+        <div >
+          <div className='whyTitle'>
+            <h2>Why choose Easybank?</h2>
+            <p>
+              We leverage Open Banking to turn your bank account into your
+              finatial hub. Control your finaces like never before.
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
